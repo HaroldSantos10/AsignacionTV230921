9c938298ddcdf14395cfc3ca51068d0deb03f0f8
Harold Santos <haroldsan9.9@gmail.com>
2021-09-24 18:54:43 -0600

    Asignación Televisor, clase POO

---
+++ AsignacionTV230921/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsignacionTV230921
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MEDIANTE METODOS DE PROPIEDAD
+            //instancia de la clase Televisor
+            Televisor televisor = new Televisor();
+            //creamos un objeto televisor
+            televisor.setTvId(1);
+            televisor.setTvMarca("LG");
+            televisor.setTvModelo("42LA6200");
+
+            //MEDIANTE METODO DE PROPIEDAD AUTOIMPLEMENTADA
+            //SET
+            televisor._tvAño = 2019;
+            televisor._tvTamaño = 50;
+            televisor._tvResolucion = "5K";
+            televisor._tvColor = "Negro";
+            
+
+
+        }
+    }
+}
---
+++ AsignacionTV230921/Televisor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignacionTV230921
+{
+    class Televisor
+    {
+        //definir miembros
+        //propiedades
+        private int _tvId;//numero de serie del televisor
+        private string _tvModelo;
+        private string _tvMarca;
+
+        //propiedades auto implementadas
+        public int _tvTamaño { get; set; }
+        public string _tvResolucion { get; set; }
+        public int _tvAño { get; set; }
+        public string _tvColor { get; set; }
+
+
+        //Metodo constructor
+        //Es un constructor vacío cuando no se introducen argumentos en el parentesis
+        public Televisor(int tvId, string tvModelo, string tvMarca, int tvTamaño, string tvResolucion, int tvAño, string tvColor)
+        {
+            _tvId = tvId;
+            _tvModelo = tvModelo;
+            _tvMarca = tvMarca;
+            _tvTamaño = tvTamaño;
+            _tvResolucion = tvResolucion;
+            _tvAño = tvAño;
+            _tvColor = tvColor;
+        }
+
+
+
+
+
+        //Metodos de propiedades
+        //get-- obtiene el valor almacenado en la propiedad
+        //set-- asignar
+        public void setTvId(int tvId)
+        {
+            _tvId = tvId;
+        }
+        public int getTvId()
+        {
+            return _tvId;
+        }
+
+        public void setTvModelo(string tvModelo)
+        {
+            _tvModelo = tvModelo;
+        }
+        public string getTvModelo()
+        {
+            return _tvModelo;
+        }
+
+        public void setTvMarca(string tvMarca)
+        {
+            _tvMarca = tvMarca;
+        }
+        public string getTvMarca()
+        {
+            return _tvMarca;
+        }
+
+
+
+    }
+}
